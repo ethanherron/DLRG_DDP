3783430031345d7f2446863e6499d8e187532621
ethanherron <edherron@iastate.edu>
2023-10-26 16:44:48 -0500

    working code

--- ddp.py
+++ ddp.py
@@ -4,53 +4,38 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-from pl_bolts.datamodules import CIFAR10DataModule
-from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+from torchvision.datasets import CIFAR10
 import torch.nn.functional as F
 from torchmetrics.functional import accuracy
 from pytorch_lightning import LightningModule, Trainer
-from pytorch_lightning.loggers import CSVLogger
 import time
 
 
 # Global variables
-PATH_DATASETS = '/work/mech-ai-scratch/DLRG_Datasets/CIFAR10'
-BATCH_SIZE = 256 if torch.cuda.is_available() else 64
-NUM_WORKERS = int(os.cpu_count() / 2)
+PATH_DATASETS = '/data/cifar10'
 
 
 def main(args):
     
-    
+    print(' ')
+    print(' prepping dataset...')
     # Global variables
-    PATH_DATASETS = '/work/mech-ai-scratch/DLRG_Datasets/CIFAR10'
-    BATCH_SIZE = args.batch_size
-    NUM_WORKERS = int(os.cpu_count() / 2)
     # Prep dataset
     train_transforms = torchvision.transforms.Compose(
         [
             torchvision.transforms.RandomCrop(32, padding=4),
             torchvision.transforms.RandomHorizontalFlip(),
             torchvision.transforms.ToTensor(),
-            cifar10_normalization(),
         ]
     )
 
-    test_transforms = torchvision.transforms.Compose(
-        [
-            torchvision.transforms.ToTensor(),
-            cifar10_normalization(),
-        ]
-    )
-
-    cifar10_dm = CIFAR10DataModule(
-        data_dir=PATH_DATASETS,
-        batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
-        train_transforms=train_transforms,
-        test_transforms=test_transforms,
-        val_transforms=test_transforms,
-    )
+    train_dataset = CIFAR10(PATH_DATASETS, train=True, transform=train_transforms, download=False)
+    
+    train_loader = torch.utils.data.DataLoader(train_dataset, 
+                                               batch_size=args.batch_size, 
+                                               shuffle=True, drop_last=True, 
+                                               num_workers=2, 
+                                               pin_memory=True)
 
     # create our model - we are just going to edit the torchvision resnet model for 32x32 cifar10 image sizes
     def create_model():
@@ -77,23 +62,6 @@ def main(args):
             self.log("train_loss", loss)
             return loss
 
-        def evaluate(self, batch, stage=None):
-            x, y = batch
-            logits = self(x)
-            loss = F.nll_loss(logits, y)
-            preds = torch.argmax(logits, dim=1)
-            acc = accuracy(preds, y)
-
-            if stage:
-                self.log(f"{stage}_loss", loss, prog_bar=True)
-                self.log(f"{stage}_acc", acc, prog_bar=True)
-
-        def validation_step(self, batch, batch_idx):
-            self.evaluate(batch, "val")
-
-        def test_step(self, batch, batch_idx):
-            self.evaluate(batch, "test")
-
         def configure_optimizers(self):
             optimizer = torch.optim.SGD(
                 self.parameters(),
@@ -104,31 +72,32 @@ def main(args):
             return {"optimizer": optimizer}
 
 
-
+    print(' ')
+    print(' creating model...')
     model = LitResnet(lr=0.05)
 
+    print(' ')
+    print(' training model...')
     trainer = Trainer(
-        max_epochs=50,
-        accelerator="gpu",
+        max_epochs=3,
+        accelerator='gpu',
         strategy='ddp', # Data Distributed Parallel Strategy! Pytorch lightning takes care of this for you!!!
-        devices=args.num_devices,  
-        logger=CSVLogger(save_dir="logs/"),
+        devices=args.num_devices
     )
 
-
     start_time = time.time()
-    trainer.fit(model, cifar10_dm)
+    trainer.fit(model, train_loader)
     end_time = time.time()
     
     print(f'{args.num_devices} GPUs with batch sizes of {args.batch_size} each took ', (end_time - start_time), ' seconds to train.')
     
     
     
-    if __name__ == '__main__':
-        parser = argparse.ArgumentParser(description='Train CIFAR 10 classification model with DDP!')
-        parser.add_argument('-b','--batch_size', default=512, type=int,
-                            help='Batch size')
-        parser.add_argument('-g','--num_devices', default=4, type=int,
-                            help='num gpus')
-        hparams = parser.parse_args()
-        main(hparams)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train CIFAR 10 classification model with DDP!')
+    parser.add_argument('-b','--batch_size', default=64, type=int,
+                        help='Batch size')
+    parser.add_argument('-g','--num_devices', default=1, type=int,
+                        help='num gpus')
+    hparams = parser.parse_args()
+    main(hparams)
